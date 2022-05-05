e19e3eaff4338482237109feda933f254154dfa0
amir <a.m.asadi2001@gmail.com>
2022-05-05 19:12:06 +0430

    add drawer styles

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -7,23 +7,40 @@ import {
   TextField,
   ToggleButtonGroup,
   ToggleButton,
+  Box,
+  Chip,
 } from "@mui/material";
 import { useState } from "react";
+import { styled } from "@mui/material/styles";
+import { grey } from "@mui/material/colors";
+
+const Puller = styled(Box)(({ theme }) => ({
+  width: 60,
+  height: 5,
+  backgroundColor: theme.palette.mode === 'light' ? grey[400] : grey[900],
+  borderRadius: 3,
+  position: 'absolute',
+  top: 8,
+  left: 'calc(50% - 30px)',
+})); 
 
 export default function Drawer({ showDrawer, setShowDrawer }) {
-  const [priority, SetPriority] = useState(1);
+  const [priority, SetPriority] = useState("low");
+  const [tags, setTags] = useState(["sag", "cat", "work"]);
+
   return (
     <SwipeableDrawer
       anchor="bottom"
       open={showDrawer}
       onClose={() => setShowDrawer(false)}
       onOpen={() => setShowDrawer(true)}
-      sx={{borderTopRightRadius: "30px"}}
+      sx={{ borderTopRightRadius: "30px" }}
     >
       <Container maxWidth="sm">
+        <Puller></Puller>
         <Stack spacing={3} alignItems="center" padding={3}>
-          <Typography variant="h4"  color="primary">
-            Add a new Task{" "}
+          <Typography variant="h5" color="primary">
+            Add a new Task
           </Typography>
           <TextField
             variant="outlined"
@@ -33,16 +50,35 @@ export default function Drawer({ showDrawer, setShowDrawer }) {
             rows="2"
           />
           <ToggleButtonGroup
-            color="primary"
             value={priority}
             exclusive
             onChange={(e) => SetPriority(e.target.value)}
+            fullWidth
+            color="info"
           >
-            <ToggleButton value={1}>Low</ToggleButton>
-            <ToggleButton value={2}>Normal</ToggleButton>
-            <ToggleButton value={3}>High</ToggleButton>
+            <ToggleButton value={"low"}>Low</ToggleButton>
+            <ToggleButton value={"normal"}>Normal</ToggleButton>
+            <ToggleButton value={"high"}>High</ToggleButton>
           </ToggleButtonGroup>
-          <Button variant="contained">Add</Button>
+          <TextField variant="outlined" label="#Tags" fullWidth size="small" />
+          <Stack
+            sx={{ width: "100%", transform: "translateY(-15px)" }}
+            direction="row"
+            justifyContent="start"
+            spacing={2}
+            flexWrap="wrap"
+          >
+            {tags.map((t) => (
+              <Chip
+                label={t}
+                onClick={() => console.log("first")}
+                onDelete={() => console.log("first")}
+              />
+            ))}
+          </Stack>
+          <Button fullWidth variant="contained">
+            Add
+          </Button>
         </Stack>
       </Container>
     </SwipeableDrawer>
