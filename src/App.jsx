54ca4b6df76414adf1b6b6680d6691fd479a0eb7
amir <a.m.asadi2001@gmail.com>
2022-05-12 17:49:59 +0430

    change default theme palette

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,42 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import Layout from "./components/Layout";
 import { createTheme } from "@mui/material/styles";
 import { useSelector } from "react-redux";
+import Layout from "./components/Layout";
+import { grey, blueGrey, blue } from "@mui/material/colors";
 
 export default function App() {
   const storedTheme = useSelector((state) => state.theme.value);
 
-  const theme = createTheme({
+  const getDesignTokens = (mode) => ({
     palette: {
-      mode: storedTheme,
-    },
-    typography: {
-      fontFamily: "verdana",
+      mode,
+      primary: {
+        ...blue,
+        ...(mode === "dark" && {
+          main: blue[300],
+        }),
+      },
+      ...(mode === "dark" && {
+        background: {
+          default: blueGrey[900],
+          paper: blueGrey[900],
+        },
+      }),
+      text: {
+        ...(mode === "light"
+          ? {
+              primary: grey[900],
+              secondary: grey[800],
+            }
+          : {
+              primary: "#fff",
+              secondary: grey[500],
+            }),
+      },
     },
   });
+
+  const theme = createTheme(getDesignTokens(storedTheme));
 
   return (
     <ThemeProvider theme={theme}>
