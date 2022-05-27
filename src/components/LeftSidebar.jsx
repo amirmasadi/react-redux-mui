66ae658f5eb9ec891c7e44c3c46f5ea236d3dd8d
amir <a.m.asadi2001@gmail.com>
2022-05-27 17:53:44 +0430

    delete old js and jsx files

--- src/components/LeftSidebar.jsx
+++
@@ -1,9 +0,0 @@
-import { Box } from "@mui/material";
-
-export default function LeftSidebar() {
-  return (
-    <Box sx={{visibility: "hidden"}}>
-      <h1>could be another sideBar</h1>
-    </Box>
-  );
-}
